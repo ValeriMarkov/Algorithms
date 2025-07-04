3a05b6bdd223b0e0cbe8306cdf47f258ac6a51c2
Valeri <v.markow.97@gmail.com>
2025-07-04 12:35:43 +0300

    BubbleSort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] train = { 1, 8, 6, 7, 3, 4, 5, 9, 2 };
+            Console.WriteLine("Original: [{0}]", string.Join(", ", train));
+            BubbleSort(train);
+            Console.WriteLine("Sorted: [{0}]", string.Join(", ", train));
+        }
+
+        public static void BubbleSort(int[] arr)
+        {
+            bool hasSwapped = true;
+            while (hasSwapped)
+            {
+                hasSwapped = false;
+
+                for (int i  = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] >  arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        hasSwapped = true;
+                    }
+                }
+            }
+        }
+    }
+}
